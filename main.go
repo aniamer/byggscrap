7b44c5f6e684e6c453ccfbfc5b2becbb61fcc482
Mohamed Amer <mohamed.amer@tink.se>
2018-11-14 15:33:58 +0100

    (byggscrap) filter apartments

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,32 +9,29 @@ import (
 	"strings"
 )
 
+type Result struct {
+	Result []ObjectInfo
+	TotalCount int
+}
+
+type ObjectInfo struct {
+	ObjectTypeDescription string
+	ObjectArea string
+	ObjectFloor string
+	RentPerMonth string
+	SeekAreaDescription string
+	StreetName string
+	EndPeriodMPDateString string
+
+}
+
 func main() {
 	client := &http.Client{}
-	values := make(url.Values)
-	json := `{"CompanyNo":-1,"SyndicateNo":1,"ObjectMainGroupNo":1,"Advertisements":[{"No":-1}],"ObjectSeekAreas":[{"No":1}],"RentLimit":{"Min":0,"Max":20000},"AreaLimit":{"Min":0,"Max":150},"ApplySearchFilter":true,"Page":1,"Take":10,"SortOrder":"","ReturnParameters":["ObjectNo","FirstEstateImageUrl","Street","SeekAreaDescription","PlaceName","ObjectSubDescription","ObjectArea","RentPerMonth","MarketPlaceDescription","CountInterest","FirstInfoTextShort","FirstInfoText","EndPeriodMP","FreeFrom","SeekAreaUrl","Latitude","Longitude","BoardNo"]}`
-	values.Set("Parm1", json)
-	values.Set("CallbackMethod","PostObjectSearch")
-	values.Set("CallbackParmCount", "1")
-	values.Set("__WWEVENTCALLBACK", "")
-	request, err := http.NewRequest("POST", "https://marknad.byggvesta.se/API/Service/SearchServiceHandler.ashx", strings.NewReader(values.Encode()))
+	values := createBody()
+	request, err := createRequest(values)
 	if err != nil {
 		fmt.Errorf("error occured %v", err)
 	}
-
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	request.Header.Set("X-Requested-With", "XMLHttpRequest")
-	request.Header.Set("Host", "marknad.byggvesta.se")
-	request.Header.Set("Referer", "https://marknad.byggvesta.se/pgSearchResult.aspx")
-	request.Header.Set("X-Momentum-API-KEY", "dIwHbOLgCS+FoZLYNYNToP9zK6VUoSgVC8BOT6cYljU=")
-	request.Header.Set("Cookie","ga=GA1.2.1142674423.1542053180; _gid=GA1.2.1223576119.1542053180; ASP.NET_SessionId=b1csjxwfo03ydyfxz3yoh1qy; Language=se")
-	request.Header.Set("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36")
-	request.Header.Set("Pragma","no-cache")
-	request.Header.Set("Origin","https://marknad.byggvesta.se")
-	request.Header.Set("Accept-Encoding","gzip, deflate, br")
-	request.Header.Set("Accept-Language","en-US,en;q=0.9,ar;q=0.8,de;q=0.7")
-	request.Header.Set("Accept","application/json,text/*")
-	request.Header.Set("Cache-Control","no-cache")
 
 	resp, err := client.Do(request)
 	defer resp.Body.Close()
@@ -46,5 +44,39 @@ func main() {
 		fmt.Errorf("error occured %v", err)
 	}
 
-	fmt.Printf("response %v", string(contents))
+	var result Result
+	jsonErr := json.Unmarshal(contents, &result)
+	if jsonErr != nil {
+		fmt.Errorf("error occured %v", err)
+	}
+
+	fmt.Printf("results %v %v", result.TotalCount, result.Result)
+}
+
+func createBody() url.Values {
+	values := make(url.Values)
+	data := `{"CompanyNo":-1,"SyndicateNo":1,"ObjectMainGroupNo":1,"HouseForms":[{"No":5}],"Advertisements":[{"No":-1}],"ObjectSeekAreas":[{"No":1}],"RentLimit":{"Min":0,"Max":20000},"AreaLimit":{"Min":0,"Max":150},"ApplySearchFilter":true,"Page":1,"Take":10,"SortOrder":"","ReturnParameters":["ObjectNo","FirstEstateImageUrl","Street","SeekAreaDescription","PlaceName","ObjectSubDescription","ObjectArea","RentPerMonth","MarketPlaceDescription","CountInterest","FirstInfoTextShort","FirstInfoText","EndPeriodMP","FreeFrom","SeekAreaUrl","Latitude","Longitude","BoardNo"]}`
+	values.Set("Parm1", data)
+	values.Set("CallbackMethod", "PostObjectSearch")
+	values.Set("CallbackParmCount", "1")
+	values.Set("__WWEVENTCALLBACK", "")
+	return values
+}
+
+func createRequest(values url.Values) (*http.Request, error) {
+	request, err := http.NewRequest("POST", "https://marknad.byggvesta.se/API/Service/SearchServiceHandler.ashx", strings.NewReader(values.Encode()))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Set("X-Requested-With", "XMLHttpRequest")
+	request.Header.Set("Host", "marknad.byggvesta.se")
+	request.Header.Set("Referer", "https://marknad.byggvesta.se/pgSearchResult.aspx")
+	request.Header.Set("X-Momentum-API-KEY", "dIwHbOLgCS+FoZLYNYNToP9zK6VUoSgVC8BOT6cYljU=")
+	request.Header.Set("Cookie", "ga=GA1.2.1142674423.1542053180; _gid=GA1.2.1223576119.1542053180; ASP.NET_SessionId=b1csjxwfo03ydyfxz3yoh1qy; Language=se")
+	request.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36")
+	request.Header.Set("Pragma", "no-cache")
+	request.Header.Set("Origin", "https://marknad.byggvesta.se")
+	request.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	request.Header.Set("Accept-Language", "en-US,en;q=0.9,ar;q=0.8,de;q=0.7")
+	request.Header.Set("Accept", "application/data,text/*")
+	request.Header.Set("Cache-Control", "no-cache")
+	return request, err
 }
